1140e3a06f70b949385239422edf55952eca1a36
Victor Costa <vg47683@gmail.com>
2024-12-02 10:40:24 -0300

    criando os arquivos

---
+++ tadFila.py
@@ -0,0 +1,21 @@
+from __future__ import annotations
+
+class Fila:
+
+    '''
+    Inicializa a fila com um tamanho n 
+    '''
+    def __init__(self):
+        raise NotImplemented
+
+    def enfileira_geral(self):
+        raise NotImplemented
+
+    def enfileira_prioridade(self):
+        raise NotImplemented
+
+    def mostra_fila(self):
+        raise NotImplemented
+
+    def vazia(self):
+        raise NotImplemented
