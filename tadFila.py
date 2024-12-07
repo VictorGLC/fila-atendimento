a5fc2f32d619488be8ef050f816cea060d61513e
Victor Costa <vg47683@gmail.com>
2024-12-07 15:31:08 -0300

    fix no merge do tadFila

--- tadFila.py
+++ tadFila.py
@@ -10,7 +10,6 @@ class Tipo(Enum):
     PRIORITARIA = auto()
 
 class Fila:
-<<<<<<< HEAD
     '''
     Inicializa a fila por ordem de chegada garantindo os atendimentos gerais e prioritários
     Os atendimentos gerais podem ser ultrapassados até duas vezes e os prioritários ficam mais 
@@ -127,42 +126,5 @@ class Fila:
         1
         >>> f.vazia()
         False
-=======
-    def __init__(self):
-        '''
-        Inicializa a fila de atendimento.
-        '''
-        raise NotImplemented
-
-    def enfileira_geral(self) -> int:
-        '''
-        Insere o código de atendimento de tipo GERAL no final da fila e retorna o código atribuído sequencialmente.
-        '''
-        raise NotImplemented
-
-    def enfileira_prioritaria(self) -> int:
-        '''
-        Insere uma nova demanda do tipo PRIORITÁRIA na fila.
-        Ela deve ser inserida antes de qualquer demanda PRIORITÁRIA ou antes de uma demanda GERAL com ultrapassagens igual a 0.
-        Durante o processo, ultrapassagens das demandas gerais ultrapassadas são decrementadas.
-        '''
-        raise NotImplemented
-
-    def mostra_fila(self) -> str:
-        '''
-        Imprime a fila de atendimentos conforme a ordem de prioridade.
-        '''
-        raise NotImplemented
-    
-    def desenfileira(self) -> int:
-        '''
-        Remove a primeira demanda da fila e retorna o seu código.
-        '''
-        raise NotImplemented
-
-    def vazia(self) -> bool:
-        '''
-        Retorna True se a fila estiver vazia, e False caso contrário.
->>>>>>> victor
         '''
         raise NotImplemented
